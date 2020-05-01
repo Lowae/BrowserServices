c38c71368279b49bea53dc88c25fa41d8314235a
Hao <1538196177@qq.com>
2020-05-01 18:29:50 +0800

    feat

--- src/Application.kt
+++ src/Application.kt
@@ -1,35 +1,49 @@
 package com.hao.browser
 
-import com.auth0.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.*
-import io.ktor.auth.jwt.*
-import org.jetbrains.exposed.dao.id.EntityID
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.StatusPages
-import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
 import io.ktor.http.Cookie
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.auth.parseAuthorizationHeader
+import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.streamProvider
 import io.ktor.jackson.jackson
 import io.ktor.request.receive
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.request.receiveMultipart
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
 import io.ktor.util.date.GMTDate
-import kotlinx.html.*
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import org.apache.http.auth.InvalidCredentialsException
-import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.Date
-import kotlin.time.ExperimentalTime
-import kotlin.time.seconds
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 
 const val jwtAuth = "jwt-auth"
 const val formAuth = "form-auth"
@@ -39,6 +53,7 @@ const val JWT_SECRET = "asdkjkajsdkjqwne"
 const val INPUT_USERNAME = "username"
 const val INPUT_PASSWORD = "password"
 const val ISSUER = "ktor-demo"
+const val SAVE_FILE_PATH = "E:\\workspace\\BrowserServices"
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -46,7 +61,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
 
     Database.connect(
-        url = "jdbc:mysql://0.0.0.0:3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai",
+        url = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false&maxReconnects=10",
         user = "root",
         password = "lh19990428",
         driver = "com.mysql.jdbc.Driver"
@@ -126,6 +141,27 @@ fun Application.module() {
         get("/") {
             call.respondText("HELLO WORLD!", ContentType.Text.Plain)
         }
+
+        post("/file") {
+            println("---------------------- file ---------------------")
+            val multiPart = call.receiveMultipart()
+            multiPart.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        val ext = File(part.originalFileName.toString()).extension
+                        val file = File(SAVE_FILE_PATH, "upload-${System.currentTimeMillis()}.$ext")
+                        part.streamProvider().use { input ->
+                            file.outputStream().buffered().use { output ->
+                                input.copyToSuspend(output)
+                            }
+                        }
+                    }
+                    else -> {
+                    }
+                }
+                part.dispose()
+            }
+        }
     }
 }
 
@@ -171,5 +207,29 @@ object UserService {
             }
         }
 
+    }
+}
+
+suspend fun InputStream.copyToSuspend(
+    out: OutputStream,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+    yieldSize: Int = 4 * 1024 * 1024,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+): Long {
+    return withContext(dispatcher) {
+        val buffer = ByteArray(bufferSize)
+        var bytesCopied = 0L
+        var bytesAfterYield = 0L
+        while (true) {
+            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+            out.write(buffer, 0, bytes)
+            if (bytesAfterYield >= yieldSize) {
+                yield()
+                bytesAfterYield %= yieldSize
+            }
+            bytesCopied += bytes
+            bytesAfterYield += bytes
+        }
+        return@withContext bytesCopied
     }
 }
